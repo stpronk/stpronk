c0acf677aff42d921aaffe65e3ea0f85f39d40e3
StPronk <stpronk@gmail.com>
2025-10-05 17:08:02 +0200

    Use asset path instead of direct path

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -11,10 +11,10 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="favicon/favicon-16x16.png">
-    <link rel="manifest" href="favicon/site.webmanifest">
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('favicon/apple-touch-icon.png') }}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicon/favicon-32x32.png') }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favicon/favicon-16x16.png') }}">
+    <link rel="manifest" href="{{ asset('favicon/site.webmanifest') }}">
 
     <script src="https://cdn.tailwindcss.com"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
@@ -133,7 +133,7 @@
             </div>
             <div class="flex justify-end">
                 <div class="w-64 h-64 rounded-full border-4 border-white dark:border-gray-700 overflow-hidden shadow-xl">
-                    <img src="images/stpronk_hero_self.jpg" alt="Steve Pronk" class="w-full h-full object-cover" />
+                    <img src="{{ asset('images/stpronk_hero_self.jpg') }}" alt="Steve Pronk" class="w-full h-full object-cover" />
                 </div>
             </div>
         </div>
