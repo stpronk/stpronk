7a4dd2793649432a8786ce910b439aec65dea8e7
StPronk <stpronk@gmail.com>
2025-10-16 10:34:13 +0200

    Website cluster now falls under the settings group

--- app/Filament/Clusters/Website/WebsiteCluster.php
+++ app/Filament/Clusters/Website/WebsiteCluster.php
@@ -5,8 +5,12 @@ namespace App\Filament\Clusters\Website;
 use BackedEnum;
 use Filament\Clusters\Cluster;
 use Filament\Support\Icons\Heroicon;
+use UnitEnum;
 
 class WebsiteCluster extends Cluster
 {
+    protected static string | UnitEnum | null $navigationGroup = 'Settings';
+    protected static ?int $navigationSort = 20;
+
     protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedSquares2x2;
 }
