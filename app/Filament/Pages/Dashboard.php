f1aa39e143b714f04a7dce476f2d04bf1d59401b
StPronk <stpronk@gmail.com>
2025-11-05 13:35:41 +0100

    Added new dashboard page for altering use on the original

---
+++ app/Filament/Pages/Dashboard.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Filament\Pages;
+
+use BackedEnum;
+use Filament\Pages\Page;
+use Filament\Pages\Dashboard as BaseDashboard;
+use Filament\Support\Icons\Heroicon;
+
+class Dashboard extends BaseDashboard
+{
+    protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedHome;
+    protected static string|BackedEnum|null $activeNavigationIcon = Heroicon::Home;
+}
