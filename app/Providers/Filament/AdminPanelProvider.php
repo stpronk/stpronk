86708031c1d37327020e234e410ae4124e656b97
StPronk <stpronk@gmail.com>
2026-03-05 21:27:43 +0100

    updated to go live

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -91,8 +91,8 @@ class AdminPanelProvider extends PanelProvider
                     ->navigationSort(21),
                 \Stpronk\Purchases\Filament\PurchasesPlugin::make()
                     ->navigationSort(22),
-                \Stpronk\Aeries\Filament\AeriesPlugin::make()
-                    ->navigationSort(23),
+//                \Stpronk\Aeries\Filament\AeriesPlugin::make()
+//                    ->navigationSort(23),
                 \Sytatsu\Boskos\Filament\BoskosPlugin::make()
                     ->navigationSort(24),
             ]);
